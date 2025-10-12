0d437587683c146999ace2c79c46206a1228a6c4
diaetr <n.m.a.h.d.t@gmail.com>
2025-10-12 20:03:43 +0200

    Comment UrlShortenerApplicationTests.java

--- src/test/java/org/example/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/org/example/urlshortener/UrlShortenerApplicationTests.java
@@ -3,11 +3,12 @@ package org.example.urlshortener;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest    // boots the Spring context to make sure the app wires up
 class UrlShortenerApplicationTests {
 
     @Test
     void contextLoads() {
+        // If the context fails to start, this test will fail.
     }
 
 }
